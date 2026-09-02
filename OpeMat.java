52f42ef54cee1f33b2cb426cef014708772a4ca2
Jaime <jgomezm@tecsup.edu.pe>
2026-09-02 15:46:15 -0500

    udpate

--- OpeMat.java
+++ OpeMat.java
@@ -3,7 +3,9 @@ public class OpeMat{
      // TO DO
      public int suma(int a, int b, int c){
 
-       int s = a + b + c ;
+       int s = a + b + c + 5 - 5 ; // REMOTO
+
+       System.out.println(s);
 
        return s;
 
